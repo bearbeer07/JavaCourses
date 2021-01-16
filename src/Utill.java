68b862bbe2de4202720f3840741d080beb286407
bearbeer07 <47346664+bearbeer07@users.noreply.github.com>
2021-01-16 12:24:59 +0300

    Delete Utill.java

--- src/Utill.java
+++
@@ -1,9 +0,0 @@
-public class Utill {
-    //перемена мест элементов
-    private void swap(int[] array, int ind1, int ind2) {
-        int tmp = array[ind1];
-        array[ind1] = array[ind2];
-        array[ind2] = tmp;
-    }
-
-}
