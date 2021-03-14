7581ccbd5506868714cc69b6498b7d457260996d
bearbeer07 <evrey1993@gmail.com>
2021-03-14 15:01:47 +0300

    решена задача с удалением дубликатов

--- src/exercise_4_3/Duplicat_map.java
+++ src/exercise_4_3/Duplicat_map.java
@@ -1,4 +1,11 @@
 package exercise_4_3;
+import java.util.Collection;
+import java.util.HashSet;
 
 public class Duplicat_map {
+    //метод где на вход подается коллекция
+    public static <T> Collection<T> removeDuplicates(Collection<T> collection) {
+        //удаляем дубликаты
+        return new HashSet<>(collection);
+    }
 }
