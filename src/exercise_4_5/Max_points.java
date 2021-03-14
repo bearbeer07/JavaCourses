2b8ca66bc1cc26472336719c62a905e417ad3355
bearbeer07 <evrey1993@gmail.com>
2021-03-14 14:46:49 +0300

    задача с игроками решена

--- src/exercise_4_5/Max_points.java
+++ src/exercise_4_5/Max_points.java
@@ -1,77 +1,46 @@
 package exercise_4_5;
 
-import java.util.HashMap;
 
 public class Max_points {
     public static String[] partners = {"Ivan 5", "Petr 3", "Alex 10", "Petr 8", "Ivan 6", "Alex 5", "Ivan 1", "Petr 5", "Alex 1"};
-    //public static HashMap<String, Integer> partners_point = new HashMap<String, Integer>();
+
     public static void main(String[] args) {
-        // HashMap<String, Integer> partners_point = new HashMap<String, Integer>();
+        //инициализируем начальное количество очков у игроков
         int ivan_score = 0;
         int petr_score = 0;
         int alex_score = 0;
+        //количество итераций с игроками
         int count_partners = partners.length;
-
+        //основной цикл для подсчета набранных очков
+        outerloop:
         for (int i = 0; i < count_partners; i++) {
             for (String val : partners[i].split(" ")) {
                 switch (val) {
-                    //case partners[i].substring(0, partners[i].indexOf(" ")):
                     case "Ivan":
                         ivan_score += Integer.parseInt(partners[i].substring(partners[i].lastIndexOf(" ")).replace(" ", ""));
+                        //выход из цикла если первый набрал Ivan
+                        if (ivan_score >= 11) {
+                            System.out.println("победил Ivan набрав " + ivan_score);
+                            break outerloop;
+                        }
                         break;
                     case "Petr":
                         petr_score += Integer.parseInt(partners[i].substring(partners[i].lastIndexOf(" ")).replace(" ", ""));
+                        //выход из цикла если первый набрал
+                        if (petr_score >= 11) {
+                            System.out.println("победил Petr набрав " + petr_score);
+                            break outerloop;
+                        }
                         break;
                     case "Alex":
                         alex_score += Integer.parseInt(partners[i].substring(partners[i].lastIndexOf(" ")).replace(" ", ""));
-                        break;
+                        //выход из цикла если первый набрал
+                        if (alex_score >= 11) {
+                            System.out.println("победил Alex набрав " + petr_score);
+                            break outerloop;
+                        }
                 }
             }
         }
     }
 }
-
-
-
-/*for(
-    String key:partners_point.keySet())
-
-    {
-        System.out.println(key + " :: " + partners_point.get(key));
-    }
-
-
-for(
-    int i = 0;
-    i<partners.length;i++)
-            for(
-    String srt :partners[i].
-
-    split(" "))
-
-    {
-
-        System.out.println(srt);//partners[i].lastIndexOf(" ")
-    }*/
-
-
-//    for (int i = 0; i < count_partners; i++) {
-
-//System.out.println(partners[i].substring(0, partners[i].indexOf(" ")));
-// partners_point.put(partners[i].substring(0, partners[i].indexOf(" ")), 0);
-//Integer.parseInt(partners[i].substring(partners[i].lastIndexOf(" ")).replace(" ", ""))
-// }
-
-
-
-    /*    for (int i = 0; i < count_partners; i++) {
-            System.out.println(partners[i].substring(0,partners[i].indexOf(" ")));
-            System.out.println(partners[i].substring(partners[i].lastIndexOf(" ")).replace(" ",""));
-             partners_point.put("ggg",6);
-
-
-             System.out.println(partners_point.values());
-
-        }*/
-
-
