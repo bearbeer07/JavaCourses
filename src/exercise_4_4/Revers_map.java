fa2337a006f875b8da9da349bec9640bba5bf6d9
bearbeer07 <evrey1993@gmail.com>
2021-03-15 11:49:53 +0300

    решена задача с переменой местами ключей и значений

--- src/exercise_4_4/Revers_map.java
+++ src/exercise_4_4/Revers_map.java
@@ -1,29 +1,34 @@
 package exercise_4_4;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Revers_map {
-
-   /* Map<String, Integer> map = new HashMap<>();
-    Map<Integer, String> swapped = map.entrySet().stream().collect(Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey));
-    ///
-    HashMap<String, String> newMap = new HashMap<>();*/
-
-
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
+        //создаем map и заполняем ее и передаем на вход в наш метод
+        HashMap<Integer, Integer> hashMap = new HashMap();
+        hashMap.put(1,2);
+        hashMap.put(1,3);
+        hashMap.put(2,2);
+        inverse(hashMap);
+    }
+    //реализация нашего метода
+    public static <K, V> Map<V, Collection<K>> inverse(Map<? extends K, ? extends V> map){
+        Map<V, Collection<K>> resultMap = new HashMap<>();
 
-        HashMap<Integer,Integer> map1 = new HashMap<>();
-        HashMap<Integer,Integer> map_revers = new HashMap<>();
-      //  Заполнение map1
-        for (int i=0; i<4; i++){
-            map1.put(i,(int) (Math.random()*10));
+       // Set<K> keys = map.keySet();
+        Set <K> keys = (Set<K>) map.keySet();
+        for(K key : keys){
+            V value = map.get(key);
+            resultMap.compute(value, (v, ks) -> {
+                if(ks == null){
+                    ks = new HashSet<>();
+                }
+                ks.add(key);
+                return ks;
+            });
         }
 
-
-
+        return resultMap;
     }
-
 
 }
